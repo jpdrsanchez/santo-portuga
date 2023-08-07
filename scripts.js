6137a9440e6a51a2675e22ad9f514541d84b2fcb
jpdrsanchez <joaopedrosanchez302@gmail.com>
2023-08-07 09:20:36 -0300

    feat: clicks section

--- scripts.js
+++ scripts.js
@@ -1,6 +1,24 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.mjs'
 
-(async () => {
+(() => {
+  const fullSwiper = new Swiper('.full-carousel', {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    breakpoints: {
+      420: {
+        slidesPerView: 2,
+      },
+      640: {
+        slidesPerView: 3,
+      },
+      1024: {
+        slidesPerView: 4,
+      }
+    }
+  })
+})()
+
+(() => {
   const singleSwiper = new Swiper('.single-space', {
     slidesPerView: 'auto',
     spaceBetween: 30,
